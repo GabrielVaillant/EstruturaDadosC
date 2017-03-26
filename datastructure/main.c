98de2f2c182f6ca68d0f120bebb2dc5dba9aa742
João Paulo Delgado Preti <joaopaulodelgadopreti@MacBook-Pro-de-Joao.local>
2017-03-26 13:09:26 -0400

    Initial Commit

---
+++ datastructure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  datastructure
+//
+//  Created by João Paulo Delgado Preti on 26/03/17.
+//  Copyright © 2017 João Paulo Delgado Preti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
