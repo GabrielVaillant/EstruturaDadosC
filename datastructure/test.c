68366a26119f8559877441a85044b5e2d1f3279f
João Paulo Delgado Preti <joaopaulodelgadopreti@MacBook-Pro-de-Joao.local>
2017-03-26 13:10:35 -0400

    Test.

---
+++ datastructure/test.c
@@ -0,0 +1,9 @@
+//
+//  test.c
+//  datastructure
+//
+//  Created by João Paulo Delgado Preti on 26/03/17.
+//  Copyright © 2017 João Paulo Delgado Preti. All rights reserved.
+//
+
+#include <stdio.h>
