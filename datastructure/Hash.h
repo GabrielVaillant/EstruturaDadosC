31a7278316d3bbcf4fe06902ada4df2e08e105a8
João Paulo Delgado Preti <jppreti@gmail.com>
2019-04-02 10:30:49 -0400

    Hash Interface

---
+++ datastructure/Hash.h
@@ -0,0 +1,30 @@
+//
+//
+//  EstruturaDeDados
+//
+//  Created by João Paulo Delgado Preti.
+//  Copyright (c) 2014 João Paulo Delgado Preti. All rights reserved.
+//
+
+#ifndef EstruturaDeDados_Hash_h
+#define EstruturaDeDados_Hash_h
+
+#include "DoublyLinkedList.h"
+
+#define MAX 10
+
+typedef struct HashStruct {
+    DoublyLinkedList hashes[MAX];
+    int size;
+}HashStruct;
+
+void initHash(HashStruct *hashStruct);
+bool IsHashEmpty(HashStruct *hashStruct);
+int hash(char *key);
+int put(HashStruct *hashStruct, char *key, void *data);
+bool containsKey(HashStruct *hashStruct, char *key);
+void* get(HashStruct *hashStruct, char *key);
+void* removeKey(HashStruct *hashStruct, char *key);
+
+#endif
+
