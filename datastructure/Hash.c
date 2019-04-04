576b1073b3e66424542de5385e97fbe1dbc3c84a
João Paulo Delgado Preti <jppreti@gmail.com>
2019-04-04 10:43:09 -0400

    Implementação de Hash

---
+++ datastructure/Hash.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Hash.h"
+
+typedef struct Cliente {
+    char nome[50];
+    char email[50];
+}Cliente;
+
+bool comparaChaves(void *key, void *data) {
+    char *chave = (char*)key;
+    Cliente *c = (Cliente*)data;
+    
+    return (strcmp (chave, c->nome) == 0)?true:false;
+}
+
+void initHash(HashStruct *hashStruct) {
+    for (int i=0;i<MAX;i++) {
+        init(&(hashStruct->hashes[i]));
+    }
+    hashStruct->size = 0;
+}
+
+bool IsHashEmpty(HashStruct *hashStruct) {
+    return hashStruct->size==0;
+}
+
+int hash(char *key) {
+    int sum = 0;
+    for (int i = 0; key[i]!=0;i++) {
+        sum+=key[i]*(i+1);
+    }
+    return sum%MAX;
+}
+
+int put(HashStruct *hashStruct, char *key, void *data)  {
+    int res = enqueue(&hashStruct->hashes[hash(key)],data);
+    hashStruct->size+=res;
+    return res;
+}
+
+bool containsKey(HashStruct *hashStruct, char *key, compare equal) {
+    int hashValue = hash(key);
+    int pos = indexOf(&hashStruct->hashes[hashValue], key, comparaChaves);
+    
+    return (pos!=-1)?true:false;
+}
+
+void* get(HashStruct *hashStruct, char *key, compare equal) {
+    int hashValue = hash(key);
+    int pos = indexOf(&hashStruct->hashes[hashValue], key, comparaChaves);
+    return getPos(&hashStruct->hashes[hashValue], pos);
+}
+
+void* removeKey(HashStruct *hashStruct, char *key, compare equal) {
+    return NULL;
+}
+
+int main() {
+    HashStruct hashes;
+    initHash(&hashes);
+    printf("%d\n",hash("Joao"));
+    int *aux = (int *)malloc(sizeof(int));
+    *aux=1;
+    put(&hashes, "Joao", aux);
+
+    Cliente *c = (Cliente *)malloc(sizeof(Cliente));
+    strcpy(c->nome,"Joao Paulo") ;
+    strcpy(c->email,"joao.preti@cba.ifmt.edu.br");
+    printf("%d\n",hash(c->nome));
+    put(&hashes, c->nome, c);
+    printf("%d\n",hashes.hashes[0].size);
+    printf("%d\n",hashes.size);
+    printf("%d\n",containsKey(&hashes, "Joao Paulo", comparaChaves));
+    
+    Cliente *cliente = (Cliente*)get(&hashes, "Joao Paulo", comparaChaves);
+    printf("%s\n",cliente->email);
+    return 0;
+}
