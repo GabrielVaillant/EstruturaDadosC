cc460f9e3569322047b6c053ba0304cf8433572b
João Paulo Delgado Preti <jppreti@gmail.com>
2019-04-18 11:03:48 -0400

    ListaGeneralizada Implementação

---
+++ datastructure/ListaGeneralizada.c
@@ -0,0 +1,9 @@
+//
+//  ListaGeneralizada.c
+//  datastructure
+//
+//  Created by João Paulo Delgado Preti on 18/04/19.
+//  Copyright © 2019 João Paulo Delgado Preti. All rights reserved.
+//
+
+#include "ListaGeneralizada.h"
